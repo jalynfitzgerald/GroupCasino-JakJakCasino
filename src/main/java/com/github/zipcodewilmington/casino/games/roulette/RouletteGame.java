da9c1a0212d69e9d5c4333da0ad785ab7b9dafba
terrik <terrik_smith@yahoo.com>
2026-08-07 13:50:38 -0400

    Implement basic Roulette game and player

--- src/main/java/com/github/zipcodewilmington/casino/games/roulette/RouletteGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/roulette/RouletteGame.java
@@ -6,15 +6,19 @@ import java.util.List;
 
 import com.github.zipcodewilmington.casino.GameInterface;
 import com.github.zipcodewilmington.casino.PlayerInterface;
+import com.github.zipcodewilmington.utils.AnsiColor;
+import com.github.zipcodewilmington.utils.IOConsole;
 
 public class RouletteGame implements GameInterface {
 
     private RouletteWheel wheel;
     private List<PlayerInterface> players;
+    private IOConsole console;
 
     public RouletteGame() {
         this.wheel = new RouletteWheel();
         this.players = new ArrayList<>();
+        this.console = new IOConsole(AnsiColor.GREEN);
     }
 
     @Override
@@ -30,15 +34,43 @@ public class RouletteGame implements GameInterface {
     @Override
     public void run() {
 
-        int winningNumber = wheel.spin();
+        console.println("====== ROULETTE ======");
+        console.println("1. Bet on Number");
+        console.println("2. Bet on Red");
+        console.println("3. Bet on Black");
 
+        String choice = console.getStringInput("Choose your bet:");
+
+        int amount = Integer.parseInt(
+                console.getStringInput("Enter bet amount:")
+        );
+
+        int winningNumber = wheel.spin();
         RouletteBetType winningColor = getWinningColor(winningNumber);
 
         for (PlayerInterface playerInterface : players) {
 
             RoulettePlayer player = (RoulettePlayer) playerInterface;
 
-            boolean winner = false;
+            if (choice.equals("1")) {
+
+                int number = Integer.parseInt(
+                        console.getStringInput("Choose a number (0-36):")
+                );
+
+                player.placeBet(RouletteBetType.NUMBER, number, amount);
+
+            } else if (choice.equals("2")) {
+
+                player.placeBet(RouletteBetType.RED, 0, amount);
+
+            } else {
+
+                player.placeBet(RouletteBetType.BLACK, 0, amount);
+
+            }
+
+            boolean winner;
 
             if (player.getBetType() == RouletteBetType.NUMBER) {
 
@@ -52,19 +84,31 @@ public class RouletteGame implements GameInterface {
 
             if (winner) {
 
-                player.getArcadeAccount()
-                        .deposit(BigDecimal.valueOf(player.getBetAmount() * 2));
+                int payout;
 
-                System.out.println("Winner!");
-                System.out.println("Winning Number: " + winningNumber);
-                System.out.println("Winning Color: " + winningColor);
+                if (player.getBetType() == RouletteBetType.NUMBER) {
+                    payout = player.getBetAmount() * 35;
+                } else {
+                    payout = player.getBetAmount() * 2;
+                }
+
+                player.getCasinoAccount()
+                        .deposit(BigDecimal.valueOf(payout));
+
+                console.println("-------------------------");
+                console.println("Winner!");
+                console.println("Winning Number: " + winningNumber);
+                console.println("Winning Color: " + winningColor);
+                console.println("You won $" + payout);
+                console.println("-------------------------");
 
             } else {
 
-                System.out.println("You Lost!");
-                System.out.println("Winning Number: " + winningNumber);
-                System.out.println("Winning Color: " + winningColor);
-
+                console.println("-------------------------");
+                console.println("You Lost!");
+                console.println("Winning Number: " + winningNumber);
+                console.println("Winning Color: " + winningColor);
+                console.println("-------------------------");
             }
         }
     }
