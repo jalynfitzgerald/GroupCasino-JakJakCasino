9c8e73da93743391ff66ad454c3732f7da1a346d
matthew <matt.gregory89@gmail.com>
2026-08-07 13:59:06 -0400

    blackjack

--- src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackjackGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackjackGame.java
@@ -3,20 +3,25 @@ package com.github.zipcodewilmington.casino.games.blackjack;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.github.zipcodewilmington.casino.CasinoAccount;
 import com.github.zipcodewilmington.casino.GameInterface;
 import com.github.zipcodewilmington.casino.PlayerInterface;
+import com.github.zipcodewilmington.casino.cards.Card;
 
 public class BlackjackGame implements GameInterface {
+
     private final List<BlackjackPlayer> players;
     private final BlackjackPlayer dealer;
     private final Shoe shoe;
 
-    public BlackjackGame(int numberOfDecks, BlackjackPlayer dealer) {
-        this.players = new ArrayList<>();
-        this.shoe = new Shoe(numberOfDecks);
-        this.shoe.shuffle();
-        this.dealer = dealer;
-    }
+    public BlackjackGame() {
+    this.players = new ArrayList<>();
+    this.shoe = new Shoe(6);
+    this.shoe.shuffle();
+
+    CasinoAccount dealerAccount = new CasinoAccount(/* whatever your constructor requires */);
+    this.dealer = new BlackjackPlayer("Dealer", dealerAccount);
+}
 
     @Override
     public void add(PlayerInterface player) {
@@ -31,6 +36,7 @@ public class BlackjackGame implements GameInterface {
             players.remove(player);
         }
     }
+
     public Card dealFromShoe() {
         return shoe.deal();
     }
@@ -40,16 +46,98 @@ public class BlackjackGame implements GameInterface {
     }
 
     public void dealInitialCards() {
+        dealer.clearHand();
+
+        for (BlackjackPlayer player : players) {
+            player.clearHand();
+        }
+
         for (int round = 0; round < 2; round++) {
+
             for (BlackjackPlayer player : players) {
                 player.addCard(dealFromShoe());
             }
+
             dealer.addCard(dealFromShoe());
         }
     }
 
+    public Card hit(BlackjackPlayer player) {
+
+        Card card = dealFromShoe();
+
+        if (card != null) {
+            player.addCard(card);
+        }
+
+        return card;
+    }
+
+    public boolean isBusted(BlackjackPlayer player) {
+        return player.getHandValue() > 21;
+    }
+
+    public void playDealerTurn() {
+
+        while (dealer.getHandValue() < 17) {
+            hit(dealer);
+        }
+    }
+
+    public String determineWinner(BlackjackPlayer player) {
+
+        int playerValue = player.getHandValue();
+        int dealerValue = dealer.getHandValue();
+
+        if (playerValue > 21) {
+            return "Dealer";
+        }
+
+        if (dealerValue > 21) {
+            return player.getPlayerName();
+        }
+
+        if (playerValue > dealerValue) {
+            return player.getPlayerName();
+        }
+
+        if (dealerValue > playerValue) {
+            return "Dealer";
+        }
+
+        return "Push";
+    }
+
+    public BlackjackPlayer getDealer() {
+        return dealer;
+    }
+
+    public List<BlackjackPlayer> getPlayers() {
+        return new ArrayList<>(players);
+    }
+
     @Override
     public void run() {
+
         dealInitialCards();
+
+        for (BlackjackPlayer player : players) {
+
+            if (!isBusted(player)) {
+                playDealerTurn();
+            }
+
+            String winner = determineWinner(player);
+
+            System.out.println(
+                    player.getPlayerName()
+                            + ": "
+                            + player.getHandValue()
+                            + " | Dealer: "
+                            + dealer.getHandValue()
+            );
+
+            System.out.println("Winner: " + winner);
+        }
     }
 }
