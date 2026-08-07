e200cb938733b796076f34e053fd45d04d02a233
matthew <matt.gregory89@gmail.com>
2026-08-07 09:16:58 -0400

    Updated code

--- src/test/java/com/github/zipcodewilmington/casino/games/blackjack/BlackjackGameTest.java
+++ src/test/java/com/github/zipcodewilmington/casino/games/blackjack/BlackjackGameTest.java
@@ -1,9 +1,9 @@
 package com.github.zipcodewilmington.casino.games.blackjack;
 
-import com.github.zipcodewilmington.casino.CasinoAccount;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.github.zipcodewilmington.casino.CasinoAccount;
 
 public class BlackjackGameTest {
 
@@ -11,7 +11,7 @@ public class BlackjackGameTest {
     public void dealInitialCards_dealsTwoCardsToPlayerAndDealer() {
         BlackjackPlayer dealer = new BlackjackPlayer(new CasinoAccount());
         BlackjackPlayer player = new BlackjackPlayer(new CasinoAccount());
-        BlackjackGame game = new BlackjackGame(1, dealer);
+        BlackjackGame game = new BlackjackGame(5, dealer);
 
         game.add(player);
         game.dealInitialCards();
